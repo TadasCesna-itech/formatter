d6cf7ef124ed084a68f1faea4b28394c690f1556
TadasCesna-itech <Tadas.Cesna@itechart-group.com>
2023-03-16 09:25:35 +0200

    moved index.ts into src dir

--- index.ts
+++
@@ -1,4 +0,0 @@
-import { getUK, getEuro } from './src/currencyFormatter/formatter';
-
-console.log(getEuro(17305));
-console.log(getUK(21055));
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { getUK, getEuro } from './currencyFormatter/formatter';
+
+console.log(getEuro(17305));
+console.log(getUK(21055));
